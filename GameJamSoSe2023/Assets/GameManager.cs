21959095e71c870b6340785fb3b2262646d67f1e
trytomatie <trytomatie@gmail.com>
2023-07-09 23:23:41 +0200

    Camera Zoom

--- GameJamSoSe2023/Assets/GameManager.cs
+++ GameJamSoSe2023/Assets/GameManager.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private int score;
-
+    private int gameTimer = 120;
     public int Score { get => score; set => score = value; }
+    public int GameTimer { get => gameTimer; set => gameTimer = value; }
+
+
 
     public static GameManager instance;
     // Start is called before the first frame update
